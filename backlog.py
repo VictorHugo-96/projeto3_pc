787baeb27c93cef05a1ed9740e84d789b83cb9d8
VictorHugo-96 <victorjovino12@gmail.com>
2026-04-28 21:34:28 -0300

    Update backlog.py

--- backlog.py
+++ backlog.py
@@ -1,25 +1,28 @@
 class FilaBacklog:
     def __init__(self):
         self.dados = []
-    
+
     def enqueue(self, jogo):
-        self.dados.append(jogo)
-    
+        if jogo not in self.dados:
+            self.dados.append(jogo)
+
     def dequeue(self):
         if self.is_empty():
             return None
         return self.dados.pop(0)
-    
+
     def is_empty(self):
         return len(self.dados) == 0
-    
+
     def mostrar(self):
         if self.is_empty():
-            print('Backlog vazio')
+            print("Backlog vazio")
             return
-        print('------BACKLOG-------')
-        for index, jogo in enumerate(self.dados, start = 1):
-            print(f'{index} - {jogo.exibir()}')
-    
+
+        print("\n--- BACKLOG ---")
+        for i, j in enumerate(self.dados, 1):
+            print(f"{i}. ", end="")
+            j.exibir()
+
     def tamanho(self):
         return len(self.dados)
